697844e737599bd495802c19b22503855879423b
Kelsey A. Robertson <krobertson@hmbnet.com>
2018-11-14 09:26:17 -0500

    Parsing Posted Form Data
    following walk through

--- HelloExpress/app.js
+++ HelloExpress/app.js
@@ -1,7 +1,21 @@
 var express = require('express'); 
 var app = express(); 
+var formidable = require('formidable');
  
 app.use('/forms', express.static(__dirname + '/public')); 
+
+app.post('/SubmitHelloPost', function(request, response){
+     if(request.method.toLowerCase() == 'post'){
+          //parse form data
+          var form = new formidable.IncomingForm();
+          form.parse(request, function(err, fields){
+               response.writeHead(200, { 'Content-Type': 'text/html' });
+               response.write('Hello ' + fields.userName + '!<br />');
+               response.end('Have a POST great day!');
+               console.log('Handled request from ' + fields.userName);
+          });
+     }
+});
  
 app.get('/SubmitHello', function (request, response) {
      response.writeHead(200, { 'Content-Type': 'text/html' });     
